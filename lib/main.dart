28162d30d3f8e0393915ddb0f1aad17cfba707d9
Prabhat MAurya <prabhumaurya880898@gmail.com>
2025-08-31 00:30:19 +0530

    animation login page statefull ,stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fprojects/pages/home_page.dart';
 import 'package:fprojects/pages/login_page.dart';
+import 'package:fprojects/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -24,8 +25,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,56 +1,104 @@
 import 'package:flutter/material.dart';
 import 'dart:developer';
 
+import 'package:fprojects/utils/routes.dart';
 
-
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool chanagedButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
-          SizedBox(height: 20.0),
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16.0,
-              horizontal: 32.0,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+            SizedBox(height: 20.0),
+            Text(
+              "$name Welcome",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter UserName",
-                    labelText: "Username",
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 32.0,
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "Username",
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: () {
-                    log("hi prabhat");
-                  },
+                  SizedBox(height: 20.0),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        chanagedButton = true;
+                      });
+                      Future.delayed(Duration(seconds: 1), () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: chanagedButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: BorderRadius.circular(
+                          chanagedButton ? 50 : 10,
+                        ),
+                      ),
+                      child: chanagedButton
+                          ? Icon(Icons.done, color: Colors.white)
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                    ),
+                  ),
 
-                  child: Text("Login"),
-                ),
-              ],
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(minimumSize: Size(200, 50)),
+                  //   onPressed: () {
+                  //     log("gone to home page ");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+
+                  //   child: Text("Login"),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
